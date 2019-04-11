8756c55c0d85f42b3ea869161b19eb7c714acd6c
Gabrielle von Koss <gabivk@gmail.com>
2019-04-11 11:42:02 +0100

    quell error

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -4,23 +4,25 @@ document.addEventListener("DOMContentLoaded", function () {
 	document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
 });
 
-// to demonstrate :invalid pseudo-states work (without user interaction)
+// to demonstrate :invalid pseudo-states work (without user interaction which is how they should work in prod)
 let invalidField = document.querySelector('.o-forms-input--invalid');
 
-let invalidTextInput = invalidField.querySelector("input[type=text]");
-if (invalidTextInput) {
-	invalidTextInput.setAttribute('required', true);
-}
-
-let invalidTextArea = invalidField.querySelector("textarea");
-if (invalidTextArea) {
-	invalidTextArea.setAttribute('required', true);
-}
-
-let invalidSelect = invalidField.querySelector("select");
-if (invalidSelect) {
-	invalidSelect.setAttribute('required', true);
-	invalidSelect[0].setAttribute('disabled', true);
-	invalidSelect[0].setAttribute('selected', true);
+if (invalidFied) {
+	let invalidTextInput = invalidField.querySelector("input[type=text]");
+	if (invalidTextInput) {
+		invalidTextInput.setAttribute('required', true);
+	}
+	
+	let invalidTextArea = invalidField.querySelector("textarea");
+	if (invalidTextArea) {
+		invalidTextArea.setAttribute('required', true);
+	}
+	
+	let invalidSelect = invalidField.querySelector("select");
+	if (invalidSelect) {
+		invalidSelect.setAttribute('required', true);
+		invalidSelect[0].setAttribute('disabled', true);
+		invalidSelect[0].setAttribute('selected', true);
+	}
 }
 
