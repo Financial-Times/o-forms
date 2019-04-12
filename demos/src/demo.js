06398b0bc9c37eb3adb5a18d1a4d225c6c8b2906
Gabrielle von Koss <gabivk@gmail.com>
2019-04-12 11:46:20 +0100

    V7: Validation (#253)
    * add js validation
    
    * add validation tests
    
    * update :invalid and :valid styles / demos
    
    * quell error
    
    * typo
    
    * gah lint
    
    * revoke individual invalid/valid styles for fields, apply generally
    
    * whoops
    
    * address PR review

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -1,10 +1,28 @@
-/*global require*/
-import './../../main.js';
+require('../../main.js');
 
-function initDemos() {
-	document.addEventListener('DOMContentLoaded', function() {
-		document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
-	});
+document.addEventListener("DOMContentLoaded", function () {
+	document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
+});
+
+// to demonstrate :invalid pseudo-states work (without user interaction which is how they should work in prod)
+let invalidField = document.querySelector('.o-forms-input--invalid');
+
+if (invalidField) {
+	let invalidTextInput = invalidField.querySelector("input[type=text]");
+	if (invalidTextInput) {
+		invalidTextInput.setAttribute('required', true);
+	}
+
+	let invalidTextArea = invalidField.querySelector("textarea");
+	if (invalidTextArea) {
+		invalidTextArea.setAttribute('required', true);
+	}
+
+	let invalidSelect = invalidField.querySelector("select");
+	if (invalidSelect) {
+		invalidSelect.setAttribute('required', true);
+		invalidSelect[0].setAttribute('disabled', true);
+		invalidSelect[0].setAttribute('selected', true);
+	}
 }
 
-initDemos();
--- src/js/forms.js
+++ src/js/forms.js
@@ -1,201 +1,100 @@
 class Forms {
+	/**
+	* Class constructor.
+	* @param {HTMLElement} [formElement] - The form element in the DOM
+	* @param {Object} [options={}] - An options object for configuring the form
+	*/
+	constructor(formElement, options) {
+		this.formEl = formElement;
+		this.inputFields = Array.from(formElement.querySelectorAll('input', 'select', 'textarea'));
 
-	constructor (FormEl, { testEvent = 'blur', applyValidState = false } = {}) {
-		this.FormEl = FormEl;
-		this.validFormEls = [
-			HTMLFormElement,
-			HTMLInputElement,
-			HTMLSelectElement,
-			HTMLButtonElement,
-			HTMLTextAreaElement
-		];
+		this.opts = Object.assign({
+			useBrowserValidation: true
+		}, options);
 
-		const declaredTestEvent = this.FormEl.getAttribute('data-o-forms-test-event');
-		const declaredApplyValidState = this.FormEl.getAttribute('data-o-forms-apply-valid-state');
-
-		this.opts = {
-			testEvent,
-			applyValidState
+		this.className = {
+			invalid: 'o-forms-input--invalid',
+			valid: 'o-forms-input--valid'
 		};
 
-		if (declaredTestEvent) {
-			this.opts.testEvent = declaredTestEvent;
-		}
-
-		if (declaredApplyValidState) {
-			this.opts.applyValidState = declaredApplyValidState === 'true';
-		}
-
-		// o-forms should only be registered against a <form>
-		// element. If not, return to prevent errors
-		if (!(this.FormEl instanceof HTMLFormElement)) {
-			this.destroy();
-			return;
-		}
-
-		// Add the event listeners
-		this.listeners();
-	}
-
-	listeners() {
-		// Setup checkbox toggle functionality.
-		// We listen for clicks as IE does not fire onchange until onblur.
-		this.FormEl.addEventListener('click', this.handleClickEvent.bind(this), false);
-
-		// Setup validity checks.
-		if (this.opts.testEvent === 'submit') {
-			// Safari reports the validity state, but doesn't
-			// prevent form submits, so this listens to submits and
-			// checks the inputs are valid before submission.
-			this.FormEl.addEventListener('submit', this.validateForm.bind(this), false);
-
-			// All other browsers will report each item invalid on
-			// submit and prevent a form submission.
-			this.findInputs().forEach(input => {
-				input.addEventListener('invalid', this.handleInvalidEvent.bind(this), false);
-			});
+		if (!this.opts.useBrowserValidation) {
+			this.formEl.setAttribute('novalidate', true);
+			this.formEl.addEventListener('submit', this);
 		} else {
-			this.findInputs().forEach((input) => {
-				input.addEventListener('blur', this.handleBlurEvent.bind(this), false);
-			});
+			this.formEl.removeAttribute('novalidate');
+			let submit = this.formEl.querySelector('input[type=submit]');
+			submit.addEventListener('click', this);
+			submit.addEventListener('keydown', this);
+		}
+
+		this.inputFields.forEach(input => {
+			input.addEventListener('blur', this);
+			input.addEventListener('input', this);
+		});
+	}
+
+	/**
+	 * Event Handler
+	 * @param {Object} event - The event emitted by element/window interactions
+	 */
+	handleEvent(e) {
+		let field = e.target.closest('.o-forms-input');
+		if (e.type === 'blur') {
+			field.querySelectorAll('input').forEach(input => this.validate(input));
+		}
+
+		if (e.type === 'input') {
+			if (e.target.validity.valid && field.classList.contains(this.className.invalid)) {
+				field.classList.replace(this.className.invalid, this.className.valid);
+			}
+		}
+
+		const RETURN_KEY = 13;
+		if (e.type === 'click' || (e.type === 'keydown' && e.key === RETURN_KEY)) {
+			if (!this.formEl.reportValidity()) {
+				this.inputFields.forEach(input => this.validate(input));
+			}
+		}
+
+		if (e.type === 'submit') {
+			e.preventDefault();
+			let validatedInputs = this.inputFields.map(input => this.validate(input));
+			if (validatedInputs.includes(false)) {
+				return;
+			}
+
+			e.target.submit();
 		}
 	}
 
-	validateForm(event) {
-		event.preventDefault();
-
-		const checkedInputs = this.findInputs().map(input => this.validateInput(input));
-
-		if (checkedInputs.some((val) => val === false)) {
-			return;
-		}
-
-		// Complete the form submission
-		event.target.submit();
-	}
-
-	validateInput(input) {
-		if (input.checkValidity() === false) {
-			this.invalidInput(input);
+	/**
+	 * Input validation
+	 * @param {Element} input - The input to validate
+	 */
+	validate(input) {
+		if (!input.validity.valid) {
+			input.closest('.o-forms-input').classList.add(this.className.invalid);
 			return false;
-		}
-
-		const oFormsEl = input.closest('.o-forms');
-		const oFormsElExists = (oFormsEl instanceof HTMLElement);
-
-		if (oFormsElExists && oFormsEl.classList.contains('o-forms--error')) {
-			oFormsEl.classList.remove('o-forms--error');
-		}
-
-		if (this.opts && this.opts.applyValidState && oFormsElExists) {
-			oFormsEl.classList.add('o-forms--valid');
 		}
 
 		return true;
 	}
 
-	invalidInput(input) { // eslint-disable-line class-methods-use-this
-		const oFormsEl = input.closest('.o-forms');
-		oFormsEl.classList.add('o-forms--error');
-		oFormsEl.classList.remove('o-forms--valid');
-		Forms.updateInputStatus(input, null); // Remove any form status e.g. "saving" or "saved".
-	}
-
-	handleInvalidEvent(event) {
-		this.invalidInput(event.target);
-	}
-
-	handleBlurEvent(event) {
-		this.validateInput(event.target);
-	}
-
-	handleClickEvent(event) { // eslint-disable-line class-methods-use-this
-		const input = event.target;
-		// Fire an event when toggle checkboxes are clicked (toggled).
-		if (input && input.hasAttribute('data-o-form-toggle')) {
-			input.dispatchEvent(new CustomEvent('oForms.toggled', {
-				bubbles: true
-			}));
-		}
-	}
-
-	findInputs() {
-		return Array.from(this.FormEl.querySelectorAll('input, select, textarea, button'));
-	}
-
-	destroy() {
-		this.FormEl.removeEventListener('submit', this.validateForm.bind(this));
-		this.findInputs().forEach(input => {
-			input.removeEventListener('invalid', this.handleInvalidEvent.bind(this));
-			input.removeEventListener('blur', this.handleBlurEvent.bind(this));
-		});
-
-		this.opts = undefined;
-		this.validFormEls = undefined;
-		this.FormEl = undefined;
-	}
-
-	static updateInputStatus (input, status) {
-		if (!(input instanceof HTMLElement)) {
-			throw new TypeError(`Expecting \`${input}\` to be an instance of "HTMLElement".`);
-		}
-		const oFormsElSelector = '.o-forms';
-		const oFormsEl = input.closest(oFormsElSelector);
-		const oFormsElExists = (oFormsEl instanceof HTMLElement);
-		const validStatuses = [
-			'saving',
-			'saved'
-		];
-		// 1. Check the status can be updated.
-		// 1a. Validate the given status is `null` or one of the whitelisted values.
-		if (status && !validStatuses.includes(status)) {
-			throw new Error(`"${status}" is not a valid status for a form input.`);
-		}
-		// 1b. Confirm the form field element is found.
-		if (!oFormsElExists) {
-			console.warn(`Could not find form field element "${oFormsElSelector}" for the given input.`, input);
-			return false;
-		}
-		// 1c. Remove status if there is an error on the form field.
-		if (oFormsElExists && status && oFormsEl.classList.contains('o-forms--error')) {
-			console.warn(`Can not update the status of an input to "${status}" when the input has an active error. Removing status.`, input);
-			Forms.updateInputStatus(input, null);
-			return false;
-		}
-		// 2. Prepare status element.
-		// 2a. Create status element if one does not exist.
-		let statusElement = oFormsEl.querySelector(`.o-forms__status`);
-		if (!statusElement) {
-			statusElement = document.createElement("div");
-			statusElement.classList.add('o-forms__status');
-			oFormsEl.append(statusElement);
-		}
-		// 2b. Add aria-live attributes if not set.
-		if (!statusElement.hasAttribute('aria-live')) {
-			statusElement.setAttribute('aria-live', 'assertive');
-			statusElement.setAttribute('role', 'alert');
-		}
-		// 3. Remove existing status.
-		statusElement.setAttribute('aria-hidden', true);
-		validStatuses.forEach((status) => {
-			oFormsEl.classList.remove(`o-forms--${status}`);
-		});
-		// 4. Set the new status.
-		if (status) {
-			oFormsEl.classList.add(`o-forms--${status}`);
-			statusElement.setAttribute('aria-hidden', false);
-		}
-		return true;
-	}
-
-	static init (rootEl, opts) {
+	/**
+	 * Initialise form component.
+	 * @param {(HTMLElement|String)} rootElement - The root element to intialise a form in, or a CSS selector for the root element
+	 * @param {Object} [options={}] - An options object for configuring the banners
+	 */
+	static init(rootEl, opts) {
 		if (!rootEl) {
 			rootEl = document.body;
 		}
+
 		if (!(rootEl instanceof HTMLElement)) {
 			rootEl = document.querySelector(rootEl);
 		}
-		if (rootEl instanceof HTMLElement && /\bo-forms\b/.test(rootEl.getAttribute('data-o-component'))) {
+
+		if (rootEl instanceof HTMLFormElement) {
 			return new Forms(rootEl, opts);
 		}
 
